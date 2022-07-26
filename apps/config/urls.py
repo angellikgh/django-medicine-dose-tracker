da79d29b54e92eb07e11d41b89becbc35261adc2
Marcus Marshall <marcus76.official@gmail.com>
2022-07-26 11:31:21 -0400

    added user login/register

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
     path('',
         TemplateView.as_view(template_name='home.html'),
         name='home'),
+    path('', include('apps.users.urls')),
 
     path('admin/', admin.site.urls),
 ]
---
+++ apps/users/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+
+from .forms import UserRegistrationForm
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            redirect_url = request.GET.get('next', 'home')
+            return redirect(redirect_url)
+        else:
+            messages.error(request, "Username Or Password is incorrect!",
+                           extra_tags='alert alert-warning alert-dismissible fade show')
+
+    return render(request, 'users/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        check1 = False
+        check2 = False
+        check3 = False
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            email = form.cleaned_data['email']
+
+            if password1 != password2:
+                check1 = True
+                messages.error(request, 'Password did not match!',
+                               extra_tags='alert alert-warning alert-dismissible fade show')
+            if User.objects.filter(username=username).exists():
+                check2 = True
+                messages.error(request, 'Username already exists!',
+                               extra_tags='alert alert-warning alert-dismissible fade show')
+            if User.objects.filter(email=email).exists():
+                check3 = True
+                messages.error(request, 'Email already registered!',
+                               extra_tags='alert alert-warning alert-dismissible fade show')
+
+            if check1 or check2 or check3:
+                messages.error(
+                    request, "Registration Failed!", extra_tags='alert alert-warning alert-dismissible fade show')
+                return redirect('users:register')
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password1, email=email)
+                messages.success(
+                    request, f'Thanks for registering {user.username}.', extra_tags='alert alert-success alert-dismissible fade show')
+                return redirect('users:login')
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'users/register.html', {'form': form})
