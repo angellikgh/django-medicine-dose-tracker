4173f23fd16feed291fa4d26cd95279058d2a954
Marcus Marshall <marcus76.official@gmail.com>
2022-07-26 08:04:47 -0400

    initial commit

---
+++ src/config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.urls import path
+
+
+urlpatterns = [
+    path('',
+        TemplateView.as_view(template_name='home.html'),
+        name='home'),
+
+    path('admin/', admin.site.urls),
+]
