ef343f1a9f292abb58d4b2429c935df0bdfb4d67
Marcus Marshall <marcus76.official@gmail.com>
2022-08-01 05:50:21 -0400

    changed home route

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.urls import path, include
 
+from apps.medicines.views import home_view
 
 urlpatterns = [
-    path('',
-        TemplateView.as_view(template_name='home.html'),
-        name='home'),
+    path('', home_view, name='home'),
     path('', include('apps.users.urls')),
+    path('medicines/', include('apps.medicines.urls')),
 
     path('admin/', admin.site.urls),
 ]
